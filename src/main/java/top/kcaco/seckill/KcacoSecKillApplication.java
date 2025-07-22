8674006b03f233bfc0fe28f8f814a200c4c88348
kcaco <1209321131@qq.com>
2025-07-22 19:16:44 +0800

    init:初始化项目

---
+++ src/main/java/top/kcaco/seckill/KcacoSecKillApplication.java
@@ -0,0 +1,13 @@
+package top.kcaco.seckill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KcacoSecKillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KcacoSecKillApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/kcaco/seckill/KcacoSecKillApplicationTests.java
@@ -0,0 +1,13 @@
+package top.kcaco.seckill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KcacoSecKillApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
